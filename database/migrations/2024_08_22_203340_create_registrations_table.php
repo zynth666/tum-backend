29ed365043be18bf408e2d3e0b3aae2c34154f7e
Dennis Eisenfeld <dennis.eisenfeld@yahoo.de>
2024-08-22 22:49:19 +0200

    Add migration for registration model

---
+++ database/migrations/2024_08_22_203340_create_registrations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('handle');
+            $table->string('group')->nullable();
+            $table->string('country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registrations');
+    }
+};
